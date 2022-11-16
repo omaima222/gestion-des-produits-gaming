0baea4642d5dc17872e34be76ef7a4fec1cede48
omaima <oumaimaelmachi@gmail.com>
2022-11-16 09:15:57 +0100

    login page front end

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin dashboard</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Jockey+One&display=swap" rel="stylesheet">
+    <link href="assets/css/vendor.min.css" rel="stylesheet" />
+   	<link href="assets/css/app.min.css" rel="stylesheet" />
+  	<link href="assets/css/style.css" rel="stylesheet" />
+</head>
+<body>
+  <header >
+    <img class="m-4" src="assets/logo.png" alt="logo">
+    <h1 class=" text-white d-flex justify-content-center fw-light ">ADMIN DASHBOARD</h1>
+  </header>
+
+
+</body>
+</html>
